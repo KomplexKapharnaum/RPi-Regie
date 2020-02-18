b4e73c7474c146e1452beb5bae1c388c3f052e79
Jeremie <jeremie@hemisphere-project.com>
2020-02-18 17:00:58 +0100

    a

--- Web Regie/script.js
+++ Web Regie/script.js
@@ -1,5 +1,35 @@
 
-  $(function() {
+$(function() {
 
+
+
+    ////////////////////////////////////////////////////////////
+    /////////////////////////  TOGGLES /////////////////////////
+    ////////////////////////////////////////////////////////////
+
+    var editionMode = true;
+    var expandedMode = true;
+    var fadeTime = 200;
+
+    $('.dispoToggle').click(function(){
+      $('.dispoMore').slideToggle(200);
+    });
+
+    $('.editToggle').click(function(){
+      if(expandedMode==true){
+        $('.seqControls').fadeOut(fadeTime,function(){
+          expandedMode=false;
+        });
+      }
+      if(expandedMode==false){
+        $('.seqControls').fadeIn(fadeTime,function(){
+          expandedMode=true;
+        });
+      }
+    });
+
+    $('.folderEditor').click(function(){
+      $('#folderOverlay').fadeIn(fadeTime);
+    });
 
 });
