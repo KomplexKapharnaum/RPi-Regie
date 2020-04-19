d700808d4d1856254be8f62f862f71d581600ca6
Server HMSPHR <admin@hmsphr.com>
2020-04-19 16:17:32 +0200

    web deploy

---
+++ pull.php
@@ -0,0 +1,14 @@
+<?php
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+}
+
+// Print the exec output inside of a pre element
+print("<pre>" . execPrint("git reset --hard HEAD && git pull") . "</pre>");
+
+
+?>
